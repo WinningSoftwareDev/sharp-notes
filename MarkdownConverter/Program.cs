1f6c90c08e040c25da2fb09ae66184669a77d207
Daniel Winning <daniel@winningsoftware.co.uk>
2026-05-09 01:42:41 +0100

    Inital MarkdownConverter Program

--- MarkdownConverter/Program.cs
+++ MarkdownConverter/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace MarkdownConverter
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
